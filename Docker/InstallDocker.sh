89d5cb94b3577b1d668f99ec4523f62385db9932
Evan <epwashkow@gmail.com>
2020-09-29 16:38:46 -0700

    Create Docker installation script

---
+++ Docker/InstallDocker.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+# https://docs.docker.com/engine/install/ubuntu/
+
+sudo apt-get update
+sudo apt-get install -y \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg-agent \
+    software-properties-common
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+
+sudo apt-get update
+sudo apt-get install -y docker-ce
+
+sudo usermod -aG docker $USER
+
+echo "Please reboot your computer now."
