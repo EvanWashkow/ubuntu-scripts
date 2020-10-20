d2ef34f59b3dfca1cca69c2471f5acf57c8500c4
Evan Washkow <epwashkow@gmail.com>
2020-10-19 20:51:42 -0700

    Hard code repository for Ubuntu 18.04

--- Docker/InstallDocker.sh
+++ Docker/InstallDocker.sh
@@ -13,9 +13,7 @@ sudo apt-get install \
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 
 sudo add-apt-repository \
-   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-   $(lsb_release -cs) \
-   stable"
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
 
 sudo apt-get update
 sudo apt-get install -y docker-ce docker-compose
