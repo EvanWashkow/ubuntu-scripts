6177f3bee5df088e7bab537cb2feab1ed00bfef0
Evan Washkow <epwashkow@gmail.com>
2021-06-11 16:47:15 -0700

    chore: update InstallDocker.sh

--- Docker/InstallDocker.sh
+++ Docker/InstallDocker.sh
@@ -3,23 +3,23 @@
 # https://docs.docker.com/engine/install/ubuntu/
 
 sudo apt-get update
-sudo apt-get install \
+sudo sudo apt-get install \
     apt-transport-https \
     ca-certificates \
     curl \
-    gnupg-agent \
-    software-properties-common
+    gnupg \
+    lsb-release
 
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 
-sudo add-apt-repository \
-   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-   $(lsb_release -cs) \
-   stable"
+echo \
+  "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
 sudo apt-get update
-sudo apt-get install -y docker-ce docker-compose
+sudo apt-get install docker-ce docker-compose
 
+sudo groupadd docker
 sudo usermod -aG docker $USER
 
 echo "Please restart your computer."
